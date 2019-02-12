9fe038b0281ed348ca88398a108c1534a78b2358
Victor W. Frye <vwfrye95@outlook.com>
2019-02-12 16:26:07 -0500

    Update HuffmanCoding.java

--- HuffmanCoding.java
+++ HuffmanCoding.java
@@ -9,47 +9,59 @@
  * @version 2.1
  * @since 1.0
  */
+import java.io.*;
+
 public class HuffmanCoding {
 
 	public static void main(String[] args) {
 		final int MAX_SIZE = 256;
-		final byte ACTION_PATH = 0;
-		final byte INPUT_FILENAME = 1;
-		final byte OUTPUT_FILENAME = 2;
+		final byte INPUT_FILENAME = 0;
+		final byte OUTPUT_FILENAME = 1;
 
-		// If the number of command line arguments is invalid, display usage
-		// error message and end process
+		// If the number of command line arguments is invalid
 		if (args.length < 2 && args.length > 3) {
+			// Display usage error message and end process
 			System.out.println("Usage:  HuffmanCoding.java [action path] [input filename] [output file name]");
 			System.exit(0);
 		}
 
-		// If the action path argument is invalid, display usage error message
-		// and end process
-		if (args[ACTION_PATH] != "compress" && args[ACTION_PATH] != "decompress") {
-			System.out.println("Usage:  HuffmanCoding.java [action path] [input filename] [output file name]");
-			System.out.println("Action can either be \'compress\' or \'decompress\'");
+		// If the input file does not exist
+		File f = new File(args[INPUT_FILENAME]);
+		if (!f.exists()) {
+			// Display input error message and end process
+			System.out.printf("The input file '%s' does not exist.\n", args[INPUT_FILENAME]);
 			System.exit(0);
 		}
-
-		// TODO: If the input file does not exist, display input error message
-		// and end process
-
-		CharacterFrequency cfArray = new CharacterFrequency[MAX_SIZE];
+			
+		// Instantiate an array of character frequency objects
+		CharacterFrequency[] cfArray = new CharacterFrequency[MAX_SIZE];
 		for (int i = 0; i < MAX_SIZE; i++) {
+			// Assign character frequency objects to array by ASCII value
 			cfArray[i] = new CharacterFrequency((char) i);
 		}
-
-		// TODO: If action path argument constitutes compression begin
-		// compression
-		if (args[ACTION_PATH] == "compress") {
-
+		
+		// TODO: Fix IO Exceptions
+		
+		// Open the input file
+		FileReader fr = new FileReader(args[INPUT_FILENAME]);
+		int value = fr.read(); // Read a character from the input file
+		
+		// If the first character is not a 0, compress.
+		if (value != 48) {
+			
+			// While not at the end of the input file
+			while (value != -1) {
+				// increment the character frequency count
+				if (value < MAX_SIZE)
+					cfArray[value].increment();
+				value = fr.read(); // Read the next character
+			}
+			
+			// TODO: Finish compression
 		}
-
-		// TODO: If the action path argument is not compression, it is by
-		// default decompression
+		// TODO: Else, decompress.
 		else {
-
+			
 		}
 	}
 }
